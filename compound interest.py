a47eb7fd62d8805ed9a648221818ebc5bcd615aa
Anirudh876 <84377246+Anirudh876@users.noreply.github.com>
2021-06-01 22:26:18 +0530

    Add files via upload

---
+++ compound interest.py
@@ -0,0 +1,8 @@
+#Here is your pycharm
+print("Enter Principal")
+inpnum1=input()
+print("Enter Rate")
+inpnum2=input()
+print("Enter Time")
+inpnum3=input()
+print("Compound Interest",int(inpnum1)*(+(inpnum2)//100)*(int(inpnum3)))
