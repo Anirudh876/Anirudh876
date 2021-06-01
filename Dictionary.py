5457c347d948441ab772ce36cc4f561e5d8fd82e
Anirudh876 <84377246+Anirudh876@users.noreply.github.com>
2021-06-01 22:24:07 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,6 @@
+print("Welcome to Dictionary")
+print("Enter the word")
+inpnum1=input()
+dictonary={"Manpower":"The number of people working", "Allocation":"An amount of a resource", "Plea":"Appeal", "Steep":"Rising or falling sharply", "Utter":"Complete or absolute", "Assured":"Confident", "Interim":"Meanwhile", "Firm":"Decided and fixed or definite", "Pre-Maturly":"Before the due time", "Dispel":"Disappear", "Stack":"More or Less oderly heap", "Apprehensive":"Anxious or fearful", "Apprise":"Inform or tell", "Ensure":"Make sure that problem does not occur", "Seamlessly":"Smoothly and Continously", "Fatigue":"Lack of energy", "Heist":"To take unlawfully, in a robbery", "Classical":"Beguiling", "Stroke":"Medical emergency", "Staple":"Enjoyed constantly", "Thrilling":"Causing excitement and pleasure", "Behold":"See or observe", "Monotony":"Lack of variety and interest"}
+print("The required meaning is")
+print(dictonary[inpnum1])
